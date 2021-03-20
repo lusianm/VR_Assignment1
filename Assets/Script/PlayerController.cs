b62c6c0c6e8fac32d1a77ca408c86225433f9792
lusianm <47287650+lusianm@users.noreply.github.com>
2021-03-20 21:27:43 +0900

    Ball Move
    UI Button을 이용한 Player Ball 움직임 구현
    Player Controller : 입력에 따른 Player 움직임 구현
    Button Manager : UI 버튼 입력을 Player Controller로 전달해주는 기능 구현

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveHorizonal;
+    public float moveVertical;
+    public float moveJump;
+
+    public float Yaxis = 0;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveHorizonal, moveJump, moveVertical);
+        rb.AddForce(movement);
+        if (transform.position.y >= Yaxis)
+        {
+            moveJump = 0.0f;                // 중력 가속도 적용(기본적으로 물체 RigidBody에서 중력 적용되어 있음)
+            Yaxis = 0;
+        }
+    }
+
+    public void Jump()
+    {
+        moveJump = 19.82f;                  // 중력 가속도 반작용 힘
+        Yaxis = transform.position.y + 1;   // 높이 조절. 
+    }
+    
+}
