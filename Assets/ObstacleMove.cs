e6603e385e4f4c9a44977cd723a780b7bd920c19
lusianm <47287650+lusianm@users.noreply.github.com>
2021-03-29 01:45:53 +0900

    Object Move by Phone Slope
    문성호 :
      - 휴대폰 좌우 기울기를 통해 Object를 움직이는 기능 구현

---
+++ Assets/ObstacleMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    CameraMove slopeCamera;
+    [SerializeField] private float moveSpeed = 3f;
+    Vector3 objectRightPosition => transform.position + transform.right.normalized * 0.52f;
+    Vector3 objectLeftPosition => transform.position - transform.right.normalized * 0.52f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slopeCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMove>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (slopeCamera.cameraSlope)
+        {
+            case 0:
+                return;
+            case 1:
+                if (!Physics.Raycast(objectRightPosition, transform.right, 0.1f, 1 << LayerMask.NameToLayer("Obstacle")))
+                    transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.right.normalized, moveSpeed);
+                break;
+            case 2:
+                if (!Physics.Raycast(objectLeftPosition, -transform.right, 0.1f, 1 << LayerMask.NameToLayer("Obstacle")))
+                    transform.position = Vector3.MoveTowards(transform.position, transform.position - transform.right.normalized, moveSpeed);
+                break;
+        }
+    }
+}
--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -6,6 +6,7 @@ public class CameraMove : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
     Gyroscope gyro;
+    public int cameraSlope = 0;
 
     // 카메라에 부착하여 카메라의 회전 및 위치를 조정하는 Script
     void Start()
@@ -13,6 +14,7 @@ public class CameraMove : MonoBehaviour
         //Gyro의 사용 허용
         gyro = Input.gyro;
         gyro.enabled = true;
+        cameraSlope = 0;
     }
 
     Quaternion GyroToUnity(Quaternion q) => new Quaternion(q.x, q.y, -q.z, -q.w);
@@ -26,14 +28,16 @@ public class CameraMove : MonoBehaviour
         playerTransform.forward = playerForward;
 
         if (transform.right.y < -0.35f)
-            Debug.Log("To Right");
+            cameraSlope = 1;
         else if (transform.right.y > 0.35f)
-            Debug.Log("To Left");
+            cameraSlope = 2;
+        else
+            cameraSlope = 0;
         //Vector3 CameraToPlayer = playerTransform.position - transform.position.normalized;
 
 
-        //가속도 센서의 정확도가 너무 약하다.
-        //cameraParent.position = transform.position - gyro.userAcceleration * Time.deltaTime*50f + new Vector3(0, Time.deltaTime/10,0);
-        //Debug.Log(Time.deltaTime);
+            //가속도 센서의 정확도가 너무 약하다.
+            //cameraParent.position = transform.position - gyro.userAcceleration * Time.deltaTime*50f + new Vector3(0, Time.deltaTime/10,0);
+            //Debug.Log(Time.deltaTime);
     }
 }
